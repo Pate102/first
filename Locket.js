3cd95f3fb22db7e7b340e60f4842841262c81414
Pate102 <lephantriduc2@gmail.com>
2026-03-19 14:27:43 +0700

    Create Locket.js

---
+++ Locket.js
@@ -0,0 +1,5 @@
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";var bumto26={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T11:52:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2025-01-22T11:52:18Z",purchase_date:"2025-01-22T11:52:17Z",store:"app_store"},cuni67={grace_period_expires_date:null,purchase_date:"2024-07-28T01:04:17Z",product_identifier:"com.bumto26.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(cuni67.product_identifier=s,obj.subscriber.subscriptions[s]=bumto26):obj.subscriber.subscriptions["com.bumto26.premium.yearly"]=bumto26,obj.subscriber.entitlements[e]=cuni67}else obj.subscriber.subscriptions["com.bumto26.premium.yearly"]=bumto26,obj.subscriber.entitlements.pro=cuni67;$done({body:JSON.stringify(obj)});
